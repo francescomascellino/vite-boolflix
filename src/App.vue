d4aca7742a25f7fa327b518b2fd4ce42c61590bf
francescomascellino <franesco.mascellino@gmail.com>
2023-09-28 16:25:11 +0200

    add search tv+ movies and flags base

--- src/App.vue
+++ src/App.vue
@@ -6,20 +6,26 @@ export default {
 
     data() {
         return {
-            store
+            store,
         }
     },
 
     mounted() {
 
-        store.searchContent(this.store.api_url);
+        // store.searchContent(this.store.api_movies);
+        // store.searchContent(this.store.api_tv);
+        store.getFlag("en")
 
     },
 
     methods: {
         startSearch() {
-            store.searchContent(this.store.api_url);
-        }
+            store.searchResult = [];
+            store.searchContent(this.store.api_movies);
+            store.searchTv(this.store.api_tv);
+            console.log("SEARCH RES", store.searchResult);
+        },
+
     }
 }
 
@@ -29,24 +35,32 @@ export default {
 <template>
     <header>
 
-        <h1 class=" text-capitalize text-info">test</h1>
+        <h1 class=" text-capitalize text-info text-center">BOOLFLIX BETA</h1>
+
         <input type="search" name="bfx-searchBar" id="bfx-searchBar" v-model="this.store.searchImput"> <button
             @click="startSearch">Search</button>
 
     </header>
 
     <main>
-        <div class="container">
-            <div class="row">
-                <div class="col-2" v-for="movie in this.store.movies">
+        <div class="container class my-4">
+            <div class="row g-4">
+                <div class="col-2" v-for="show in this.store.searchResult">
 
                     <div class="card">
                         <img class="card-img-top" src="https://picsum.photos/200" alt="">
                         <div class="card-body">
-                            <p class="card-title">{{ movie.title }}</p>
-                            <p class="card-text">{{ movie.original_title }}</p>
-                            <p class="card-text">{{ movie.original_language }}</p>
-                            <p class="card-text">{{ movie.vote_average }}</p>
+                            <p>`{{ show.title ? show.title : show.name }}`</p>
+                            <p>`{{ show.original_title ? show.original_title : show.original_name }}`</p>
+                            <p class="card-text">{{ show.original_title }}</p>
+                            <p class="card-text">{{ show.original_language }}</p>
+                            <p class="card-text">{{ show.vote_average }}</p>
+                            <img class="bfx-flag" :alt="show.original_language"
+                                :src="this.store.getFlag(show.original_language)" />
+
+                            <!-- <img class="bfx-flag" :alt="show.original_language"
+                                :src="'https://flagsapi.com/' + `${this.store.getFlag(show.original_language)}` + '/shiny/64.png'" /> -->
+
                         </div>
                     </div>
 
@@ -57,4 +71,8 @@ export default {
     </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.bfx-flag {
+    height: 24px;
+}
+</style>
--- src/store.js
+++ src/store.js
@@ -5,11 +5,21 @@ import axios from "axios";
 export const store = reactive({
 
     api_key: "df0571431ac3bb84d4126e111a35cb87",
-    api_url: "https://api.themoviedb.org/3/search/movie", // MOVIES
+    api_movies: "https://api.themoviedb.org/3/search/movie", // MOVIES
+    api_tv: "https://api.themoviedb.org/3/search/tv", // TV SERIES
 
     movies: [], //response DELLA CHIAMATA AXIOS
+    tv: [],
+    searchResult: [],
 
-    searchImput: "",
+    flags: [
+        {
+            lang: "en",
+            url: "https://flagsapi.com/US/shiny/64.png"
+        }
+    ],
+
+    searchImput: "Star Wars",
 
     searchContent(url) {
         axios.get(url, {
@@ -24,14 +34,48 @@ export const store = reactive({
         })
             .then(response => {
 
-                console.log(response);
                 this.movies = response.data.results;
-                console.log(this.movies);
+                this.searchResult.push(...response.data.results);
+                console.log("MOVIES", this.movies);
 
             })
             .catch(error => {
                 console.error(error);
             })
     },
+
+    searchTv(url) {
+        axios.get(url, {
+
+            params: {
+                api_key: this.api_key,
+                query: this.searchImput,
+                include_adult: false,
+                language: "it-IT"
+            }
+
+        })
+            .then(response => {
+
+                this.tv = response.data.results;
+                this.searchResult.push(...response.data.results);
+                console.log("TV", this.tv);
+
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    },
+
+    /*     getFlag(iso) {
+            return iso.toUpperCase()
+        } */
+
+    getFlag(language) {
+        let index = this.flags.findIndex(flag => flag.lang == language);
+        return this.flags[index].url;
+    }
+
+
 
 })
