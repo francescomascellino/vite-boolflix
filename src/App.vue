d60b9947bb79456f1ea8c26c0ffafed7806fa3a6
francescomascellino <franesco.mascellino@gmail.com>
2023-09-28 13:09:26 +0200

    add axios calls and searchbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
-<script setup>
+<script>
+
+import { store } from './store';
+
+export default {
+
+    data() {
+        return {
+            store
+        }
+    },
+
+    mounted() {
+
+        store.searchContent(this.store.api_url);
+
+    },
+
+    methods: {
+        startSearch() {
+            store.searchContent(this.store.api_url);
+        }
+    }
+}
+
 
 </script>
 
 <template>
     <h1 class=" text-capitalize text-info">test</h1>
+    <input type="search" name="bfx-searchBar" id="bfx-searchBar" v-model="this.store.searchImput"> <button
+        @click="startSearch">Search</button>
 </template>
 
 <style lang="scss" scoped></style>
--- src/store.js
+++ src/store.js
@@ -4,6 +4,38 @@ import axios from "axios";
 
 export const store = reactive({
 
+    api_key: "df0571431ac3bb84d4126e111a35cb87",
+    api_url: "https://api.themoviedb.org/3/search/movie", // MOVIES
 
+    movies: [], //response DELLA CHIAMATA AXIOS
+    /*     movieTitle: "",
+        movieOriginalTitle: "",
+        movieLang: "",
+        movieVote: Number(""), */
+
+    searchImput: "",
+
+    searchContent(url) {
+        axios.get(url, {
+
+            params: {
+                api_key: this.api_key,
+                query: this.searchImput,
+                include_adult: false,
+                language: "it-IT"
+            }
+
+        })
+            .then(response => {
+
+                console.log(response);
+                this.movies = response.data.results;
+                console.log(this.movies);
+
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    },
 
 })
